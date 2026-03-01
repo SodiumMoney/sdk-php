3cedc0549367654de239a87fe29d65e989efac89
Alex Cupra <alexcupra1123@gmail.com>
2026-03-01 21:40:25 +0000

    test: add blknoiz06 payment vectors

--- tests/AddressesTest.php
+++ tests/AddressesTest.php
@@ -12,4 +12,19 @@ final class AddressesTest extends TestCase
     {
         $this->assertSame('3WYJaf1ocPSbQAMjFhka5t8iusdH11Atw6FcxeuVYsTw', Addresses::getConfigAddress('mainnet'));
     }
+
+    public function testPaymentBlknoiz06(): void
+    {
+        $this->assertSame(
+            'HPT9k5YWkKNQcZGewGmABjRu8A5Bnem6VsBmNrNsSgyd',
+            Addresses::getPaymentAddress('twitter', '@blknoiz06')
+        );
+    }
+
+    public function testPaymentAlias(): void
+    {
+        $a = Addresses::getPaymentAddress('twitter', '@blknoiz06');
+        $b = Addresses::getPaymentAddress('twitter', '@BLKNOIZ06');
+        $this->assertSame($a, $b);
+    }
 }
